ece46eea3b42051ac68aa8ce37260b9e9b0f717c
jorgensena <jorgensena@middleton.school.nz>
2022-02-11 14:26:37 +1300

    quiz

---
+++ 0.9_question_17.py
@@ -0,0 +1,30 @@
+
+times = []
+time = int(input("Enter a time for the 200m race (or '-1' to exit): "))
+total_t = 0
+num = 0
+fastest = 1000
+
+# get times
+while time != -1:
+    times.append(time)
+
+    fastest = 0
+    if time < fastest:
+        fastest = time
+
+    total_t += time
+    num += 1
+
+    time = int(input("Enter a time for the 200m race (or '-1' to exit): "))
+
+
+#calculate avg
+avg_t = total_t / num
+
+# output
+for speed in times:
+    print(speed)
+
+print(f"The fastest time is: {fastest}s")
+print(f"The average time is: {avg_t:.2f}s")
